5ec7412f9a31b2a753faa5cf0daf54160e6692f1
Mongezi <momasombuka@gmail.com>
2025-07-29 02:29:06 +0300

    update hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <header className="text-center py-20 px-4 bg-white dark:bg-gray-900 text-white transition-colors duration-300">
       <h2 className="text-4xl font-bold mb-4 text-blue-500 dark:text-white">
-        Hey, I&apos;m Mo 👋
+        Hey, I&apos;m Mongezi 👋
       </h2>
       <p className="text-lg text-black dark:text-gray-300 max-w-2xl mx-auto mb-6">
         I&apos;m passionate about building clean, scalable web applications and
