9101c4fcf1258d42818dd9484b7f6fc7c5231ccc
Mongezi <momasombuka@gmail.com>
2025-07-29 23:34:20 +0300

    Fix project type option

--- components/ProjectAddForm.tsx
+++ components/ProjectAddForm.tsx
@@ -40,7 +40,7 @@ const ProjectAddForm = () => {
           className="border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800 text-black dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full py-2 px-3"
           required
         >
-          <option value="Fullsatck">Fullsatck</option>
+          <option value="Fullsatck">Fullstack</option>
           <option value="Frontend">Frontend</option>
           <option value="Backend">Backend</option>
         </select>
